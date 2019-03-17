22f6596e5dac52fbc4954f238dfc741aa250c4d9
Mesut Tasci <slackermesut@gmail.com>
2019-03-18 01:14:27 +0300

    api: read base url from config

--- apps/api/lib/api.ex
+++ apps/api/lib/api.ex
@@ -23,12 +23,10 @@ defmodule Api do
   end
 
   defp build_short_url(conn, url_map) do
-    scheme = Atom.to_string(conn.scheme)
-    host = conn.host
-    port = Integer.to_string(conn.port)
+    base_url = Application.get_env(:web, :base_url)
     hash = Map.get(url_map, :hash)
 
-    "#{scheme}://#{host}:#{port}/#{hash}"
+    "#{base_url}/#{hash}"
   end
 
 end
