07b9117a86fc9928bfe2cba07a0a910d065a62d8
Mesut Tasci <slackermesut@gmail.com>
2019-03-25 04:13:19 +0300

    read DB URL at application start

--- apps/core/lib/repo.ex
+++ apps/core/lib/repo.ex
@@ -2,4 +2,10 @@ defmodule Core.Repo do
   use Ecto.Repo,
     otp_app: :core,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    # Reading DATABASE_URL on application start
+    url = System.get_env("DATABASE_URL")
+    if url, do: {:ok, [url: url] ++ config}, else: {:ok, config}
+  end
 end
