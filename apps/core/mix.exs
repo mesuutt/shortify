77d267b372aeb22e6a43e67d2f0b654c652845a2
Mesut Tasci <slackermesut@gmail.com>
2019-03-30 13:59:24 +0300

    core app paths updated

--- apps/core/mix.exs
+++ apps/core/mix.exs
@@ -5,6 +5,10 @@ defmodule Core.MixProject do
     [
       app: :core,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
